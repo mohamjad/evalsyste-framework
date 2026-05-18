ceebe456d50ac3d3a06b976d94a8dd09d7ebb6ec
mohamjad <mohammedamjad526@gmail.com>
2026-05-18 08:48:14 -0400

    core: start --init--

---
+++ evalsyste/__init__.py
@@ -0,0 +1,11 @@
+"""Public API for the evalsyste evaluation package."""
+
+from evalsyste.models import (
+    Criterion,
+    EvalCase,
+    EvalReport,
+    EvalResult,
+    EvidenceItem,
+    ModelTurn,
+    SessionTrace,
+)
